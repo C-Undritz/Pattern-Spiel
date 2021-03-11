42beb757303ba756f1b7ea05699f9eff16242282
C-Undritz <undritz.cu@gmail.com>
2021-03-11 16:59:04 +0000

    feat: added player timer to record time taken to complete pattern and submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,283 @@
-let playButton = document.getElementById("play-game");
+document.addEventListener("DOMContentLoaded", function() {
+  let container = document.getElementById("container");
+
+  let mainRow = document.createElement("div");
+  mainRow.setAttribute('class', 'row d-flex align-items-center');
+  mainRow.setAttribute('id', 'game-score-row');
+  container.appendChild(mainRow);
+
+  let gameColumn = document.createElement("div");
+  gameColumn.setAttribute('class', 'col-12 order-2 d-flex align-items-center flex-column');
+  gameColumn.setAttribute('id', 'game-column');
+  mainRow.appendChild(gameColumn);
+  
+  let newRowOne = document.createElement("div");
+  newRowOne.setAttribute('class', 'row title-row position-one-menu');
+  newRowOne.setAttribute('id', 'position-one');
+  let newRowTwo = document.createElement("div");
+  newRowTwo.setAttribute('class', 'row position-two-menu');
+  newRowTwo.setAttribute('id', 'position-two');
+  let newRowThree = document.createElement("div");
+  newRowThree.setAttribute('class', 'row position-three-menu');
+  newRowThree.setAttribute('id', 'position-three');
+  let newRowFour = document.createElement("div");
+  newRowFour.setAttribute('class', 'row position-four-menu');
+  newRowFour.setAttribute('id', 'position-four');
+
+  gameColumn.appendChild(newRowOne);
+  gameColumn.appendChild(newRowTwo);
+  gameColumn.appendChild(newRowThree);
+  gameColumn.appendChild(newRowFour);
+  
+  let gameTitle =`
+    <h1>Pattern Spiel!</h2>
+  `
+
+  let mainMenuButtons =`
+  <button onclick="selectDifficulty()">Start</button>
+  <button>How to Play</button>
+  `
+  document.getElementById('position-one').innerHTML = gameTitle;
+  document.getElementById('position-three').innerHTML = mainMenuButtons;
+})
+
 let gridSize;
 let colourPalette;
 let viewTimerSelected;
-let playerTimerSelected;
 let userColourSelected;
 let computerPattern;
 let gameRound = 1;
 let startedGame = false;
+var timer; 
+var milliseconds = 0; 
+var seconds = 0; 
+var minutes = 0;
+let totalScore;
+let patternDisplayTimer;
+
+function mainMenu(gameStarted) {
+  console.log(gameStarted);
+  document.getElementById('position-one').innerHTML = "";
+  document.getElementById('position-two').innerHTML = "";
+  document.getElementById('position-three').innerHTML = "";
+  document.getElementById('position-four').innerHTML = "";
+
+  if (gameStarted) {
+    let scoreColumn = document.getElementById("score-column");
+    scoreColumn.remove();
+  }
+
+  let gameColumn = document.getElementById("game-column");
+  gameColumn.removeAttribute('class');
+  gameColumn.setAttribute('class', 'col-12 order-2 d-flex align-items-center flex-column');
+
+  let rowOne = document.getElementById("position-one");
+  rowOne.removeAttribute('class');
+  rowOne.setAttribute('class', 'row position-one-menu');
+  
+  let rowTwo = document.getElementById("position-two");
+  rowTwo.removeAttribute('class');
+  rowTwo.setAttribute('class', 'row position-two-menu');
+
+  let rowThree = document.getElementById("position-three");
+  rowThree.removeAttribute('class');
+  rowThree.setAttribute('class', 'row position-three-menu');
+
+  let rowFour = document.getElementById("position-four");
+  rowFour.removeAttribute('class');
+  rowFour.setAttribute('class', 'row position-four-menu');
+
+  let gameTitle =`
+    <h1>Pattern Spiel!</h2>
+  `;
+
+  let mainMenuButtons =`
+  <button onclick="selectDifficulty()">Start</button>
+  <button>How to Play</button>
+  `;
+  document.getElementById('position-one').innerHTML = gameTitle;
+  document.getElementById('position-three').innerHTML = mainMenuButtons;
+}
+
+function selectDifficulty() {
+  let selectDifficultyText =`
+  <h2>Select Difficulty</h2>
+  `;
+  let difficultySelectionButtons =`
+  <button class="" id="" onclick="setDifficultyVariables(1)">Very easy</button>
+  <button class="" id="" onclick="setDifficultyVariables(2)">Easy</button>
+  <button class="" id="" onclick="setDifficultyVariables(3)">Medium</button>
+  <button class="" id="" onclick="setDifficultyVariables(4)">Hard</button>
+  <button class="" id="" onclick="setDifficultyVariables(5)">Very hard</button>
+  <button class="" id="" onclick="gridSizeSelection()">Custom game</button>
+  <button class="" id="" onclick="mainMenu(false)">return</button>
+  `;
+  document.getElementById('position-two').innerHTML = selectDifficultyText;
+  document.getElementById('position-three').innerHTML = difficultySelectionButtons
+}
+
+function setDifficultyVariables(difficulty) {
+  switch (difficulty) {
+    case 1:
+      gridSize = 3;
+      colourPalette = 4;
+      viewTimerSelected = 15;
+      break;
+    case 2:
+      gridSize = 3;
+      colourPalette = 4;
+      viewTimerSelected = 10;
+      break;
+    case 3:
+      gridSize = 3;
+      colourPalette = 4;
+      viewTimerSelected = 5;
+      break;
+    case 4:
+      gridSize = 3;
+      colourPalette = 5;
+      viewTimerSelected = 10;
+      break;
+    case 5:
+      gridSize = 3;
+      colourPalette = 5;
+      viewTimerSelected = 5;
+      break;
+  }
+  console.log("Grid size is " + gridSize);
+  console.log("Colour selection is " + colourPalette);
+  console.log("Timer selected is " + viewTimerSelected);
+  playGame(viewTimerSelected, true);
+}
 
 function gridSizeSelection() {
+    let selectOptionsText =`
+      <h2>Select grid size</h2>
+    `;
     let optionButtons = `
     <button class="gameOptionsButton" id="3" onclick="colourNumberSelection(3)">3 x 3 grid</button>
     <button class="gameOptionsButton" id="4" onclick="colourNumberSelection(4)">4 x 4 grid</button>
+    <button class="" id="" onclick="selectDifficulty()">return</button>
     `;
-    document.getElementById('game-options').innerHTML = optionButtons;
+    document.getElementById('position-two').innerHTML = selectOptionsText;
+    document.getElementById('position-three').innerHTML = optionButtons;
 }
-
-playButton.addEventListener('click', gridSizeSelection);
 
 function colourNumberSelection(gridSelection) {
     gridSize = gridSelection;
+    let selectOptionsText =`
+      <h2>Select number of possible colours</h2>
+    `;
     let optionButtons = `
     <button class="gameOptionsButton" id="4" onclick="viewTimerSelection(4)">Up to 4 colours</button>
     <button class="gameOptionsButton" id="5" onclick="viewTimerSelection(5)">Up to 5 colours</button>
     <button class="gameOptionsButton" id="6" onclick="viewTimerSelection(6)">Up to 6 colours</button>
+    <button class="" id="" onclick="gridSizeSelection()">return</button>
     `;
-    document.getElementById('game-options').innerHTML = optionButtons;
+    document.getElementById('position-two').innerHTML = selectOptionsText;
+    document.getElementById('position-three').innerHTML = optionButtons;
 }
 
 function viewTimerSelection(colourSelection) {
     colourPalette = colourSelection;
-    let optionButtons = `
-    <button class="gameOptionsButton" id="0" onclick="playerTimerSelection(0)">No Timer</button>
-    <button class="gameOptionsButton" id="5" onclick="playerTimerSelection(5)">5 Seconds</button>
-    <button class="gameOptionsButton" id="10" onclick="playerTimerSelection(10)">10 seconds</button>
-    <button class="gameOptionsButton" id="15" onclick="playerTimerSelection(15)">15 seconds</button>
+    let selectOptionsText =`
+      <h2>Select how long you will be able to view the pattern</h2>
     `;
-    document.getElementById('game-options').innerHTML = optionButtons;
+    let optionButtons = `
+    <button class="gameOptionsButton" id="0" onclick="playGame(0, false)">No Timer</button>
+    <button class="gameOptionsButton" id="5" onclick="playGame(5, false)">5 Seconds</button>
+    <button class="gameOptionsButton" id="10" onclick="playGame(10, false)">10 seconds</button>
+    <button class="gameOptionsButton" id="15" onclick="playGame(15, false)">15 seconds</button>
+    <button class="" id="" onclick="colourNumberSelection(gridSize)">return</button>
+    `;
+    document.getElementById('position-two').innerHTML = selectOptionsText;
+    document.getElementById('position-three').innerHTML = optionButtons;
 }
 
-function playerTimerSelection(viewTimerSelection) {
+function playGame(viewTimerSelection, gameChoice) {
     viewTimerSelected = viewTimerSelection;
-    let optionButtons = `
-    <button class="gameOptionsButton" id="0" onclick="playGame(0)">No Timer</button>
-    <button class="gameOptionsButton" id="10" onclick="playGame(10)">10 Seconds</button>
-    <button class="gameOptionsButton" id="15" onclick="playGame(15)">15 seconds</button>
-    <button class="gameOptionsButton" id="20" onclick="playGame(20)">20 seconds</button>
-    `;
-    document.getElementById('game-options').innerHTML = optionButtons;
-}
-
-function playGame(playerTimerSelection) {
-    playerTimerSelected = playerTimerSelection;
+    
     console.log(gridSize);
     console.log(colourPalette);
     console.log(viewTimerSelected);
-    console.log(playerTimerSelected);
-    let optionButtons = `
-    <button class="gameOptionsButton" id="0" onclick="generateComputerGrid()">Play Game!</button>
+    console.log(gameChoice);
+    
+    let selectOptionsText =`
+      <h2>Ready?</h2>
     `;
-    document.getElementById('game-options').innerHTML = optionButtons;
+    
+    if (gameChoice) {
+      let optionButtons = `
+      <button class="gameOptionsButton" id="0" onclick="generateComputerGrid()">Play Game!</button>
+      <button class="" id="" onclick="selectDifficulty()">return</button>
+      <button class="" id="" onclick="mainMenu(false)">Main menu</button>
+      `;
+      document.getElementById('position-two').innerHTML = selectOptionsText;
+      document.getElementById('position-three').innerHTML = optionButtons;
+    } else {
+      let optionButtons = `
+      <button class="gameOptionsButton" id="0" onclick="generateComputerGrid()">Play Game!</button>
+      <button class="" id="" onclick="viewTimerSelection(colourPalette)">return</button>
+      <button class="" id="" onclick="mainMenu(false)">Main menu</button>
+      `;
+      document.getElementById('position-two').innerHTML = selectOptionsText;
+      document.getElementById('position-three').innerHTML = optionButtons;
+    }
+    totalScore = 0;
 }
 
 function generateComputerGrid() {
     if (startedGame) {
         let palette = document.getElementById('palette-area'); 
-        palette.remove(); // removes palette when viewing computer generated pattern between rounds.
-    }
-
-    if (startedGame) {
         let message = document.getElementById('messages-box'); 
+        palette.remove(); // removes palette when viewing computer generated pattern between rounds.      
         message.remove(); // removes end of round message generated by function gameStatus().
+        var milliseconds = 0; 
+        var seconds = 0; 
+        var minutes = 0;;
     }
-        console.log(gridSize);
+    
+    document.getElementById('position-one').innerHTML = "";
+    document.getElementById('position-two').innerHTML = "";
+    document.getElementById('position-three').innerHTML = "";
+
+    let mainRow = document.getElementById("game-score-row");
+
+    let scoreColumn = document.createElement("div");
+    scoreColumn.setAttribute('class', 'col-2 order-1 d-flex align-items-center flex-column');
+    scoreColumn.setAttribute('id', 'score-column');
+    mainRow.appendChild(scoreColumn);
+    scoreColumn.innerHTML = "hello"
+
+    let gameColumn = document.getElementById("game-column")
+    gameColumn.removeAttribute('class');
+    gameColumn.setAttribute('class', 'col-10 order-2 d-flex align-items-center flex-column');
+
+    let rowOne = document.getElementById("position-one");
+    rowOne.removeAttribute('class');
+    rowOne.setAttribute('class', 'row position-one-game');
+    
+    let rowTwo = document.getElementById("position-two");
+    rowTwo.removeAttribute('class');
+    rowTwo.setAttribute('class', 'row position-two-game');
+
+    let rowThree = document.getElementById("position-three");
+    rowThree.removeAttribute('class');
+    rowThree.setAttribute('class', 'row position-three-game');
+
+    let rowFour = document.getElementById("position-four");
+    rowFour.removeAttribute('class')
+    rowFour.setAttribute('class', 'row position-four-game');
+    
+    let scoreColumnTitle =`
+    <button onclick="mainMenu(true)">Main menu</button>
+    <h1>Score</h2>
+    `;
+
+    document.getElementById('score-column').innerHTML = scoreColumnTitle;
+
+    console.log(gridSize);
+    
     let grid = `
     <div class="container${gridSize}x${gridSize}">
     `;
@@ -85,7 +290,7 @@ function generateComputerGrid() {
     grid += `
         </div>
     `;
-    document.getElementById('game-options').innerHTML = grid;
+    document.getElementById('position-two').innerHTML = grid;
     addPattern();
     viewTimer();
 }
@@ -127,9 +332,9 @@ function addPattern() {
 // timer code adapted from 'Code with Ania Kubów' Youtube channel video: "Build your own COUNTDOWN TIMER in 15 lines of JavaScript code" (https://www.youtube.com/watch?v=vSV_Ml2_A88&t=19s)
 // with help from Bim who informed me that set interval should be declared as a variable
 function viewTimer() {
-    let timerDisplay = document.getElementById("game-triggers");
-    let currentTimer = document.createElement("h3");
-    currentTimer.setAttribute('class', 'current-timer');
+    let timerDisplay = document.getElementById("position-one");
+    let currentTimer = document.createElement("h2");
+    currentTimer.setAttribute('id', 'current-timer');
     timerDisplay.appendChild(currentTimer);
 
     let timeLeft = viewTimerSelected;
@@ -141,11 +346,15 @@ function viewTimer() {
         }
         currentTimer.innerHTML = timeLeft;
         timeLeft -=1;
+        console.log(timeLeft);
     }, 1000);
 }
 
 function generatePlayerGrid() {
     console.log(gridSize); 
+    
+    document.getElementById('position-one').innerHTML = "";
+    
     let grid = `
     <div class="container${gridSize}x${gridSize}">
     `;
@@ -158,8 +367,27 @@ function generatePlayerGrid() {
     grid += `
         </div>
     `;
-    document.getElementById('game-options').innerHTML = grid;
+    document.getElementById('position-two').innerHTML = grid;
+
     generatePalette();
+    playerTimer() 
+}
+
+//https://www.youtube.com/watch?v=oY8V6GuZrkM 
+function playerTimer() {
+  playerTimer = setInterval(run, 10); // 10 so that the function is called 100 times a second.
+}
+
+function run() { 
+    milliseconds++;
+    if(milliseconds == 100) {
+        milliseconds = 0;
+        seconds++;
+    }
+    if(seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
 }
 
 function generatePalette() {
@@ -178,7 +406,7 @@ function generatePalette() {
     palette += `
         </div>
     `;
-    document.getElementById('right-column').innerHTML = palette;
+    document.getElementById('position-three').innerHTML = palette;
     generateSubmitButton();
 }
 
@@ -186,7 +414,7 @@ function generateSubmitButton() {
     console.log("generating submit button");
     let submitButton = `
     <button id="submit-button" onclick="userPatternArray()">Submit</button>`;
-    document.getElementById('game-triggers').innerHTML = submitButton;
+    document.getElementById('position-four').innerHTML = submitButton;
 }
 
 function pickColour(colourChosen) {
@@ -208,7 +436,7 @@ function addColour(identifier, colourChosen) {
 function userPatternArray() {
     var gridOfSquares = document.getElementsByClassName('squares');
     squareCount = gridOfSquares.length;
-    console.log(squareCount);
+    console.log("The square count is " + squareCount);
     let userPattern = [];
     for (let i = 0; i < squareCount; i++) {
         if (gridOfSquares[i].style.backgroundColor === "red") {
@@ -240,12 +468,17 @@ function gameStatus(userPattern) {
     console.log('the user pattern is: ' + userPattern);
     console.log('the AI pattern is: ' + computerPattern);
     let score = 0;
-    let totalScore;
     let result;
 
     let submitButton = document.getElementById('submit-button');
     submitButton.remove();
+    debugger;
 
+    clearInterval(timer)//-----------------------------------------------------------------------------------------------------
+    console.log("You took " + minutes + " minutes");
+    console.log("You took " + seconds + " seconds");
+    console.log("You took " + milliseconds + " miliseconds");
+    debugger;
     for (i = 0; i <computerPattern.length; i++) {
         if (userPattern[i] === computerPattern[i]) {
             score += 1;
@@ -253,7 +486,7 @@ function gameStatus(userPattern) {
     }
 
     console.log("the result is " + score);
-
+    debugger;
     if (score === userPattern.length) {
         result = "Win";
         console.log("you win");
@@ -266,19 +499,25 @@ function gameStatus(userPattern) {
         console.log("You got " + ((score/userPattern.length)*100) +"% right");
         console.log("your total score is " + totalScore);
     }
+    debugger;
 
     //Adds result of the round to the score column
     let displayResults = document.getElementById("score-column");
     let newScore = document.createElement("h3");
+    let playerTime = document.createElement("h2");
     newScore.innerHTML = result;
+    playerTime.innerHTML = s + ":" + ms;
     newScore.setAttribute('class', 'result-entry');
+    playerTime.setAttribute('class', 'result-entry');
     displayResults.appendChild(newScore);
+    displayResults.appendChild(playerTime);
 
 	//Opens up the end of round message box for player to trigger next round or to notify end of current game.
     if (gameRound < 6) {
         let message = `
         <div id="messages-box">
             <h2>You got a ${result}</h2>
+            <h2>Your time: ${s}:${ms}</h2>
             <h3>Ready for round ${gameRound}</h3>
             <button onclick="generateComputerGrid()">Next Round</button>
         </div>
@@ -319,5 +558,9 @@ function playAgain() {
         let heading = document.getElementsByClassName('result-entry')[0];
         heading.remove();
     }
+    m = 0;
+    s = 0;
+    ms = 0;
     generateComputerGrid();
 }
+
